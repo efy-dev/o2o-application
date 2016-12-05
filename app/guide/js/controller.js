f07837def74aa8d5b3756577d301592fa9ef776b
jiuxuan00 <117898271@qq.com>
2016-12-05 14:41:00 +0800

    导航固定

--- app/guide/js/controller.js
+++ app/guide/js/controller.js
@@ -4,12 +4,12 @@
  * @returns {string}
  */
 function renderHeader(it /**/) {
-    var out = ' <a href="javascript:history.go(-1)" class="angle left" title="返回上一页"></a> <h1 class="text_hidden">' + (it.title) + '</h1><button class="icon user" title="个人中心" style="border: none;"></button> <div class="layer"><a class="link" href="/app/index.html"><i class="icon icon-home"></i>首页</a><a class="link" href="http://i.efeiyi.com/"><i class="icon icon-center"></i>个人中心</a><i class="angle"></i></div>';
+    var out = ' <a href="javascript:history.go(-1)" class="angle left" title="返回上一页"></a> <h1 class="text_hidden">' + (it.title) + '</h1><button class="icon user" title="个人中心" style="border: none;"></button> <div class="layer"><a class="link" href="index.html"><i class="icon icon-home"></i>首页</a><a class="link" href="http://i.efeiyi.com/"><i class="icon icon-center"></i>个人中心</a><i class="angle"></i></div>';
     return out;
 }
 
 function renderHomeHeader(it /**/) {
-    var out = ' <a href="http://www.efeiyi.com/app/index.html" class="icon logo_efeiyi" title="e飞蚁"></a><a href="http://www.efeiyi.com/app/index.html" class="icon logo" title="前门 华韵"></a> <button class="icon user" title="个人中心" style="border: none;" ></button> <div class="layer"> <a class="link" href="/app/index.html"><i class="icon icon-home"></i>首页</a> <a class="link" href="http://i.efeiyi.com/"><i class="icon icon-center"></i>个人中心</a> <i class="angle"></i> </div>';
+    var out = ' <a href="http://www.efeiyi.com/app/index.html" class="icon logo_efeiyi" title="e飞蚁"></a><a href="http://www.efeiyi.com/app/index.html" class="icon logo" title="前门 华韵"></a> <button class="icon user" title="个人中心" style="border: none;" ></button> <div class="layer"> <a class="link" href="index.html"><i class="icon icon-home"></i>首页</a> <a class="link" href="http://i.efeiyi.com/"><i class="icon icon-center"></i>个人中心</a> <i class="angle"></i> </div>';
     return out;
 }
 

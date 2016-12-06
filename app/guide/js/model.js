28500a41c8953319d56b745415fc0988af6e3372
jiuxuan00 <117898271@qq.com>
2016-12-06 10:43:31 +0800

    店铺详情： 1.所有商品样式修改 2.增加筛选

--- app/guide/js/model.js
+++ app/guide/js/model.js
@@ -1,5 +1,5 @@
-//var api_url = 'http://192.168.1.10';
-var api_url = '';
+// var api_url = '';
+var api_url = 'http://192.168.1.10';
 // var api_url = 'http://192.168.1.72';
 var PageVariable = {
     template: {

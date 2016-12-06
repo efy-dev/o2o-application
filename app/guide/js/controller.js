4fe6cfad1cf32daada5548da5ff1b1aad1f5cf8a
jiuxuan00 <117898271@qq.com>
2016-12-06 18:25:33 +0800

    搜索部分修改 筛选部分修改

--- app/guide/js/controller.js
+++ app/guide/js/controller.js
@@ -14,7 +14,7 @@ function renderHomeHeader(it /**/) {
 }
 
 function renderFooter(it /**/) {
-    var out = ' <footer> <div class="bd item"><h3>战略合作伙伴</h3> <div class="bd icons"><span><a href="http://en.unesco.org/" title="联合国教科文组织"><em class="bd"><i class="icon-home icon1"></i></em>联合国教科文组织</a></span><span><a href="http://mall.efeiyi.com" title="非物质文化遗产平台"><em class="bd"><i class="icon-home icon2"></i></em>非物质文化遗产平台</a></span><span><a title="中国非物质文化遗产保护协会"><em class="bd"><i class="icon-home icon3"></i></em>中国非遗保护协会</a></span></div> </div> <div class="bd content"> <div class="bd wechat"> <div class="bd"> <div class="icon-home icon-logo"></div> </div> <div class="bd"><h4>非物质文化遗产平台</h4></div> <div class="bd img"><img src="http://www.efeiyi.com/app/images/icon-home-wechat.png"></div> <div class="bd txt"><p>关注微信公众号</p> <p>领取超值代金券</p></div> </div> </div> <div class="bd copyright">京ICP备15032511号-1</div> </footer>';
+    var out = ' <i class="searchbg"></i><footer> <div class="bd item"><h3>战略合作伙伴</h3> <div class="bd icons"><span><a href="http://en.unesco.org/" title="联合国教科文组织"><em class="bd"><i class="icon-home icon1"></i></em>联合国教科文组织</a></span><span><a href="http://mall.efeiyi.com" title="非物质文化遗产平台"><em class="bd"><i class="icon-home icon2"></i></em>非物质文化遗产平台</a></span><span><a title="中国非物质文化遗产保护协会"><em class="bd"><i class="icon-home icon3"></i></em>中国非遗保护协会</a></span></div> </div> <div class="bd content"> <div class="bd wechat"> <div class="bd"> <div class="icon-home icon-logo"></div> </div> <div class="bd"><h4>非物质文化遗产平台</h4></div> <div class="bd img"><img src="http://www.efeiyi.com/app/images/icon-home-wechat.png"></div> <div class="bd txt"><p>关注微信公众号</p> <p>领取超值代金券</p></div> </div> </div> <div class="bd copyright">京ICP备15032511号-1</div> </footer>';
     return out;
 }
 
@@ -24,22 +24,27 @@ function renderFooter(it /**/) {
 $().ready(function () {
     initWx("http://www.efeiyi.com/wx/init.do");
 
-    var $header=$("[dot-template=header]");
+    var $header = $("[dot-template=header]");
     $header.on("touchend", '.search', function (e) {
-        $(this).addClass('active');
+        var _this = $(this);
+        _this.addClass('active');
+        $('.searchbg').show();
+        $('.searchbg').on('click', function () {
+            _this.removeClass('active');
+            $(this).hide();
+        });
         // e.preventDefault();
     });
 
-
-
+    //右侧菜单
     $header.on("touchend", '.icon.user', function (e) {
         var iconUser = $('.icon.user');
         var layer = $('.layer');
 
-        if(layer.is(':hidden')){
+        if (layer.is(':hidden')) {
             iconUser.addClass('active');
             layer.show();
-        }else{
+        } else {
             layer.hide();
             iconUser.removeClass('active');
         }
